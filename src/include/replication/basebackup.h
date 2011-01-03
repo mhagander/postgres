9e19e4f9360329889f50558cd117b914696a0cbe
Magnus Hagander <magnus@hagander.net>
2011-01-03 19:36:43 +0100

    Oops, forgot to "git add" the header file

---
+++ src/include/replication/basebackup.h
@@ -0,0 +1,17 @@
+/*-------------------------------------------------------------------------
+ *
+ * basebackup.h
+ *	  Exports from replication/basebackup.c.
+ *
+ * Portions Copyright (c) 2010-2010, PostgreSQL Global Development Group
+ *
+ * $PostgreSQL$
+ *
+ *-------------------------------------------------------------------------
+ */
+#ifndef _BASEBACKUP_H
+#define _BASEBACKUP_H
+
+extern void SendBaseBackup(char *backup_label);
+
+#endif   /* _BASEBACKUP_H */
