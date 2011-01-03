989777bc9aa9e13f42903399dbbd28b1036f25b8
Magnus Hagander <magnus@hagander.net>
2011-01-03 13:00:26 +0100

    Update the (C) date - that's the easy part :-)

--- src/backend/replication/basebackup.c
+++ src/backend/replication/basebackup.c
@@ -3,7 +3,7 @@
  * basebackup.c
  *	  code for taking a base backup and streaming it a standby
  *
- * Portions Copyright (c) 2010-2010, PostgreSQL Global Development Group
+ * Portions Copyright (c) 2010-2011, PostgreSQL Global Development Group
  *
  * IDENTIFICATION
  *	  $PostgreSQL$
