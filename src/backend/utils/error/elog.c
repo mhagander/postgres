6a51f4dfe86e65e176eddd9f0eb77e9955913ff4
Magnus Hagander <magnus@hagander.net>
2011-10-25 19:56:34 +0200

    Remove unnecessary comment

--- src/backend/utils/error/elog.c
+++ src/backend/utils/error/elog.c
@@ -123,7 +123,7 @@ static void write_syslog(int level, const char *line);
 static void write_console(const char *line, int len);
 
 #ifdef WIN32
-extern char *event_source; /* guc */
+extern char *event_source;
 static void write_eventlog(int level, const char *line, int len);
 #endif
 
