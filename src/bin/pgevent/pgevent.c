fcbab9c452dc75c5470fef0c54c4642f4b808004
Magnus Hagander <magnus@hagander.net>
2011-10-25 18:57:39 +0200

    Spelling and spacing fix

--- src/bin/pgevent/pgevent.c
+++ src/bin/pgevent/pgevent.c
@@ -48,10 +48,12 @@ DllInstall(BOOL bInstall,
 			pszCmdLine, sizeof(event_source));
 
 	/*
-	 * This is an ugry part due to the strange behavior of "regsvr32 /i".
+	 * This is an ugly hack due to the strange behavior of "regsvr32 /i".
+	 *
 	 * When installing, regsvr32 calls DllRegisterServer before DllInstall.
 	 * When uninstalling (i.e. "regsvr32 /u /i"), on the other hand,
 	 * regsvr32 calls DllInstall and then DllUnregisterServer as expected.
+	 *
 	 * This strange behavior forces us to specify -n (i.e. "regsvr32 /n /i").
 	 * Without -n, DllRegisterServer called before DllInstall would
 	 * mistakenly overwrite the default "PostgreSQL" event source registration.
